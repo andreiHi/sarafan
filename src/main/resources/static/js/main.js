024c45f46635c24f43b04d4b6f0c290ec0073445
andreiHi <andreih1981@gmail.com>
2019-01-08 23:07:38 +0200

    Web Socket

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,8 +3,12 @@ import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
 import { connect } from "./util/ws"
 
-connect()
+if (frontendData.profile) {
+    connect()
+}
+
 Vue.use(VueResource)
+
 new Vue({
     el: '#app',
     render: a => a(App)
